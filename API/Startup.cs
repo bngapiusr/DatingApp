4228d67511bfee265d0463761221bc81cdb58dc3
Tony Ochiche <aochiche@msn.com>
2021-06-10 08:23:29 -0500

    End of section 3...

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,6 +43,7 @@ namespace API
             }
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -62,6 +63,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
